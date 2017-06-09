cf4d1ad18292011c42898f6e22fba7ea194f0391
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-09 16:46:59 +0900

    Hello3

--- firstReact/react-app/src/App.js
+++ firstReact/react-app/src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return React.createElement('h1', null, 'Hello Egg');
-  }
-}
+// class App extends Component {
+//   render() {
+//     return React.createElement('h1', null, 'Hello Egg');
+//   }
+// }
+
+const App = () => <h1>Hello</h1>
 
 export default App;
