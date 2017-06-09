ac8983ef7a944a6e318bbe0e138f7b8abc0fa2a6
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-09 16:45:00 +0900

    Hello2

--- firstReact/react-app/src/App.js
+++ firstReact/react-app/src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <h1>Hello World</h1>
-    );
+    return React.createElement('h1', null, 'Hello Egg');
   }
 }
 
